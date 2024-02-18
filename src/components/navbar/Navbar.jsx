aa18dffa9d915de47e8a08b91fb8fa3e3a0ad05f
alifzwan <alifzakwan529@gmail.com>
2024-02-19 00:31:31 +0800

    Added Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import styles from "./navbar.module.scss"
+import Image from 'next/image'
+import Link from 'next/link'
+import ThemeToggle from '../themeToggle/ThemeToggle'
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>
+
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={30} height={30}></Image>
+        <Image src="/instagram.png" alt="instagram" width={30} height={30}></Image>
+        <Image src="/tiktok.png" alt="tiktok" width={30} height={30}></Image>
+        <Image src="/youtube.png" alt="youtube" width={30} height={30}></Image>
+      </div>
+
+      <div className={styles.logo}>
+        Alif Zakwan
+      </div>
+
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Login</Link>
+      </div>
+
+
+
+    </div>
   )
 }
 
