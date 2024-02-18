b882d3656af55c80f670b446bd3e6e0fdebd7cd3
alifzwan <alifzakwan529@gmail.com>
2024-02-19 05:39:39 +0800

    Added Card.jsx and styling

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from "./card.module.scss"
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+
+        <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+        </div>
+
+        <div className={styles.textContainer}>
+            <div className={styles.details}>
+                <span className={styles.date}>11.02.2023 - </span>
+                <span className={styles.category}>Culture</span>
+            </div>
+            <Link href="/">
+                <h1>Full-Stack Blog App Tutorial | Next.js MongoDB Blog App Project Full Course</h1>
+            </Link>
+
+            <p className={styles.description}>Next.js MongoDB Blext.js MongoDB Blext.js MongoDB Bl
+            ext.js MongoDB Blext.js MongoDB Blext.js MongoDB Blv
+            vext.js MongoDB Blvext.js MongoDB Blext.js MongoDB Bl
+            </p>
+            
+            <Link href="/" className={styles.link}>Read More</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Card
