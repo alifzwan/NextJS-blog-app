c502340a1b2b32282a6ea3b6f7cc614ff734d90e
alifzwan <alifzakwan529@gmail.com>
2024-02-19 00:31:48 +0800

    Added AuthLinks (Log In)

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./authLinks.module.scss"
+
+const AuthLinks = () => {
+  return (
+    <div className={styles.container}>AuthLinks
+    </div>
+  )
+}
+
+export default AuthLinks
