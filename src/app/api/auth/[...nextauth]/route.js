eac68008863434b6be826ddae7b3f91d190ad995
alifzwan <alifzakwan529@gmail.com>
2024-02-19 16:49:21 +0800

    Create API Route

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,9 @@
+import { authOption } from "@/utils/auth"
+import NextAuth from "next-auth"
+
+
+const handler =  NextAuth(authOption)
+
+export {handler as GET, handler as POST}
+
+// All requests to /api/auth/* (signIn, callback, signOut, etc.) will automatically be handled by NextAuth.js.
