c695d47efc7f730e26eddddf9ed509a753cdb92d
alifzwan <alifzakwan529@gmail.com>
2024-02-19 16:54:28 +0800

    added useSession to view the authentication (POST, GET) process

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,26 +1,32 @@
+"use client"
 import React from 'react'
 import styles from "./loginPage.module.scss"
 import Image from 'next/image'
+import { signIn, useSession } from 'next-auth/react'
 
 const LoginPage = () => {
-  return (
-    <div className={styles.container}>
-        <div className={styles.wrapper}>
-            <div className={styles.socialButton}>
-                <Image src="/google.png" width={50} height={50} className={styles.logo}/>
-                Sign in with Google
-            </div>
-            <div className={styles.socialButton}>
-               <Image src="/github.png" width={50} height={50} className={styles.logo}/>
-                Sign in with Github
-            </div>
-            <div className={styles.socialButton}>
-            <Image src="/facebook.png" width={50} height={50} className={styles.logo}/>
-                Sign in with Facebook
+
+    const {data, status} = useSession()
+    console.log(data, status)
+    
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.socialButton} onClick={() => signIn("google")}>
+                    <Image src="/google.png" alt ="" width={50} height={50} className={styles.logo}/>
+                    Sign in with Google
+                </div>
+                <div className={styles.socialButton}>
+                <Image src="/github.png" alt ="" width={50} height={50} className={styles.logo}/>
+                    Sign in with Github
+                </div>
+                <div className={styles.socialButton}>
+                <Image src="/facebook.png" alt ="" width={50} height={50} className={styles.logo}/>
+                    Sign in with Facebook
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default LoginPage
