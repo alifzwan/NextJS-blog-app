6e8af7e3d662ce935d3a8ef8e8147071f0e02db5
alifzwan <alifzakwan529@gmail.com>
2024-02-19 10:24:17 +0800

    Added hover color

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,28 +9,25 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
 
-      <div className={styles.social}>
-        <Image src="/facebook.png"  alt="facebook"  width={30} height={30}></Image>
-        <Image src="/instagram.png" alt="instagram" width={30} height={30}></Image>
-        <Image src="/tiktok.png"    alt="tiktok"    width={30} height={30}></Image>
-        <Image src="/youtube.png"   alt="youtube"   width={30} height={30}></Image>
-      </div>
+        <div className={styles.social}>
+          <Image src="/facebook.png"  alt="facebook"  width={30} height={30}></Image>
+          <Image src="/instagram.png" alt="instagram" width={30} height={30}></Image>
+          <Image src="/tiktok.png"    alt="tiktok"    width={30} height={30}></Image>
+          <Image src="/youtube.png"   alt="youtube"   width={30} height={30}></Image>
+        </div>
 
-      <div className={styles.logo}>
-        Alif Zakwan
-      </div>
+        <div className={styles.logo}>
+          Alif Zakwan
+        </div>
 
-      <div className={styles.links}>
-        <ThemeToggle />
-        <Link href="/" className={styles.links}>Homepage</Link>
-        <Link href="/" className={styles.links}>Contact</Link>
-        <Link href="/" className={styles.links}>About</Link>
-        <Link href="/" className={styles.links}>Login</Link>
-        <AuthLinks />
-      </div>
-
-
-
+        <div className={styles.links}>
+            <ThemeToggle />
+            <Link href="/" className={styles.links}>Homepage</Link>
+            <Link href="/" className={styles.links}>Contact</Link>
+            <Link href="/" className={styles.links}>About</Link>
+            <Link href="/" className={styles.links}>Login</Link>
+            <AuthLinks />
+        </div>
     </div>
   )
 }
