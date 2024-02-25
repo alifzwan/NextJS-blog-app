69cabd3aeec07e4502804b49e185216f91620398
alifzwan <alifzakwan529@gmail.com>
2024-02-25 18:59:54 +0800

    added cat params

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -5,11 +5,16 @@ export const GET = async (req) => {
 
   const { searchParams } = new URL(req.url)
   const page = searchParams.get("page")
+  const cat = searchParams.get("cat")
+
   const POST_PER_PAGE = 2;
 
   const query = {
     take: POST_PER_PAGE,
-    skip: POST_PER_PAGE * (page - 1)
+    skip: POST_PER_PAGE * (page - 1),
+    where: {
+      ...(cat && { catSlug: cat })
+    }
   }
 
 
@@ -19,7 +24,9 @@ export const GET = async (req) => {
     
     const [posts, count] = await prisma.$transaction([ 
       prisma.post.findMany(query),
-      prisma.post.count()
+      prisma.post.count({ 
+        where: query.where 
+      })
     ]);
 
     if(posts){
