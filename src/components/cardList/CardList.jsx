b424311b4e45f5183628a777f4621ffdb18f5c67
alifzwan <alifzakwan529@gmail.com>
2024-02-25 19:00:17 +0800

    added cat params

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -3,8 +3,8 @@ import styles from "./cardList.module.scss"
 import Pagination from '../pagination/Pagination'
 import Card from '../card/Card'
 
-const getData = async (page) => {
-  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`,{
+const getData = async (page, cat) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,{
     cache: "no-store"
   })
 
@@ -16,9 +16,9 @@ const getData = async (page) => {
 
 
 
-const CardList = async ({page}) => {
+const CardList = async ({page, cat}) => {
 
-  const {posts, count} = await getData(page)
+  const {posts, count} = await getData(page, cat)
   const POST_PER_PAGE = 2;
 
   const hasPrev     = POST_PER_PAGE * (page - 1) > 0
