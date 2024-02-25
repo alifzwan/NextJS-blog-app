17f7b2ad314fa5b40880aee406a1b330566769c8
alifzwan <alifzakwan529@gmail.com>
2024-02-25 19:33:05 +0800

    fetch the data from mongodb for /posts

---
+++ src/app/api/posts/[slug]/route.js
@@ -0,0 +1,29 @@
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+// GET SINGLE POTS
+export const GET = async (req, {params}) => {
+
+    const {slug} = params
+ 
+try{
+
+    const post = await prisma.post.findUnique({
+        where: { slug: slug },
+        include: { user: true}  //We user in model Post. There's a relationship between em 
+                                
+    })
+    
+    
+
+    return new NextResponse(
+        JSON.stringify(post, { status: 200 })
+    );
+  } catch (error) {
+    console.log(error);
+
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,55 +4,68 @@ import Menu from '@/components/menu/Menu'
 import Image from 'next/image'
 import Comments from '@/components/comments/Comments'
 
-const SinglePage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`,{
+    cache: "no-store"
+  })
+
+  if(!res.ok){
+    throw new Error("Failed")
+  }
+  return res.json()
+}
+
+
+
+const SinglePage = async ({params}) => {
+
+  const {slug} = params
+
+  const data = await getData(slug)
+
+
   return (
     <div className={styles.container}>
         <div className={styles.infoContainer}>
 
             <div className={styles.textContainer}>
 
-                <h1 className={styles.title}>Next.js MongoDB Blog App Project Full Course</h1>
+                <h1 className={styles.title}>
+                 {data?.title}
+                </h1>
 
                 <div className={styles.user}>
-                    <div className={styles.userImageContainer}>
-                      <Image src="/p1.jpeg" alt="" fill className={styles.avatar}/>
-                    </div>
+
+                    {data?.user?.image && (
+                      <div className={styles.userImageContainer}>
+                        <Image src={data.user.image} alt="" fill className={styles.avatar}/>
+                      </div>
+                    )}
                     
                     <div className={styles.userTextContainer}>
-                      <span className={styles.username}>John Doe</span>
-                      <span className={styles.date}>01.01.2024</span>
+                      <span className={styles.username}>{data?.user.name}</span>
+                      <span className={styles.date}>{data.createdAt.substring(0,10)}</span>
                     </div>
                 </div>
+
             </div>
 
-            <div className={styles.imageContainer}>
-              <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
-            </div>
+            {data?.img && (
+              <div className={styles.imageContainer}>
+                <Image src={data.img} alt="" fill className={styles.image}/>
+              </div>
+            )}
+           
         </div>
 
 
 
         <div className={styles.content}>
               <div className={styles.post}>
-                  <div className={styles.description}>
+                  <div className={styles.description} 
+                  dangerouslySetInnerHTML={{ __html: data?.desc }}
+                  />
                     
-                      <h2>Next.js MongoDB Blog App Project</h2>
-                      <p>Next.js MongoDB Blog App Project Full CourseNext.js 
-                        MongoDB Blog App Project Full CourseNext.js 
-                        MongoDB Blog App Project Full Course
-                      </p>
-                      <h2>Next.js MongoDB Blog App Project</h2>
-                      <p>Next.js MongoDB Blog App Project Full CourseNext.js 
-                        MongoDB Blog App Project Full CourseNext.js 
-                        MongoDB Blog App Project Full Course
-                      </p>
-                      <h2>Next.js MongoDB Blog App Project</h2>
-                      <p>Next.js MongoDB Blog App Project Full CourseNext.js 
-                        MongoDB Blog App Project Full CourseNext.js 
-                        MongoDB Blog App Project Full Course
-                      </p>
-                  </div>
-                  
                   <div className={styles.comments}>
                     <Comments />
                   </div>
