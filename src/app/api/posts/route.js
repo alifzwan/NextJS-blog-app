82a22f4ff62497999d34496bf5f022acd8bd52c0
alifzwan <alifzakwan529@gmail.com>
2024-02-26 20:32:18 +0800

    Added POST function to publish blog on /write

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
@@ -16,9 +17,6 @@ export const GET = async (req) => {
       ...(cat && { catSlug: cat })
     }
   }
-
-
-
 
   try {
     
@@ -41,5 +39,36 @@ export const GET = async (req) => {
     return new NextResponse(
       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
     );
+  }
+};
+
+
+export const POST = async (request) => {
+
+  const session = await getAuthSession();
+
+  if (!session) {
+      return new NextResponse(
+          JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+      );
+  }
+
+  try {
+      const body = await request.json();
+      const post = await prisma.post.create({
+          data: { ...body, userEmail: session.user.email },
+      });
+  
+      return new NextResponse(
+          JSON.stringify(post, { status: 200 })
+      );
+
+  } catch (err) {
+
+      console.log(err);
+
+      return new NextResponse(
+          JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+      );
   }
 };
